bbcc9a761c48e41fec0c103794ae0cbf9746405b
Duong Hong Minh <dhm090804@gmail.com>
2024-12-13 13:30:38 +0700

    Bo sung splash screen va anh splash screen

--- App.js
+++ App.js
@@ -1,11 +1,11 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import AppNavigator from './src/navigation/AppNavigator';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import AppNavigator from "./src/navigations/AppNavigator";
 export default function App() {
   return (
     <NavigationContainer>
-          <AppNavigator /> // Điều hướng màn hình chính
+      <AppNavigator />
     </NavigationContainer>
   );
 }
@@ -13,8 +13,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
--- src/navigations/AppNavigator.js
+++ src/navigations/AppNavigator.js
@@ -1,30 +1,31 @@
-import { View, Text } from 'react-native'
-import React from 'react'
-import { createTop } from '@react-navigation/bottom-tabs';
-import Icon from 'react-native-vector-icons/Ionicons';
-import Homestack from './Homestack';
-import Profilestack from './Profilestack';
-import Shopstack from './Shopstack';
+import React from "react";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import Icon from "react-native-vector-icons/Ionicons";
+import Homestack from "./Homestack";
+import Profilestack from "./Profilestack";
+import Shopstack from "./Shopstack";
 const Tab = createBottomTabNavigator();
 export default function Tabnavigator() {
   return (
-   <Tabnavigator
-   screenOptions={({ route }) => ({
-     tabBarIcon: ({ focused, color, size }) => {
-       let iconName;
-       if (route.name === 'Home') {
-         iconName = focused ? 'home' : 'home-outline';
-       } else if (route.name === 'Profile') {
-         iconName = focused ? 'person' : 'person-outline';
-       }else{
-          iconName = focused ? 'cart' : 'cart-outline';
-       }
-       return <Icon name={iconName} size={size} color={color} />;
-     },
-   })}>
-        <Tab.Screen name="Home" component={Homestack} />
-        <Tab.Screen name="Shop" component={Shopstack} />
-        <Tab.Screen name="Profile" component={Profilestack}/>
-   </Tabnavigator>
-  )
+    <Tabnavigator
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ focused, color, size }) => {
+          let iconName;
+          if (route.name === "Home") {
+            iconName = focused ? "home" : "home-outline";
+          } else if (route.name === "Profile") {
+            iconName = focused ? "person" : "person-outline";
+          } else {
+            iconName = focused ? "cart" : "cart-outline";
+          }
+          return <Icon name={iconName} size={size} color={color} />;
+        },
+      })}
+    >
+      <Tab.Screen name="Home" component={Homestack} />
+      <Tab.Screen name="Shop" component={Shopstack} />
+      <Tab.Screen name="Profile" component={Profilestack} />
+      
+    </Tabnavigator>
+  );
 }
